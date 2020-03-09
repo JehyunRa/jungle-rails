1db850c7f011e8abb2fe8235196378a0b1dca159
JehyunRa <jra4862@gmail.com>
2020-03-09 15:21:49 +0000

    product-specs improved validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'product'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    product = Product.new(
+      name: 'new item',
+      price: 100,
+      quantity: 10,
+      category: Category.new
+    )
+
+    it 'should save when all fields are valid' do
+      expect(product).to be_valid
+    end
+
+    it 'should have a name' do
+      @product = product
+      @product.name = nil
+      expect(@product).to be_invalid
+    end
+
+    it 'should have a price' do
+      @product = product
+      @product.price = nil
+      expect(@product).to be_invalid
+    end
+
+    it 'should have a quantity' do
+      @product = product
+      @product.quantity = nil
+      expect(@product).to be_invalid
+    end
+
+    it 'should have a category' do
+      @product = product
+      @product.category = nil
+      expect(@product).to be_invalid
+    end
+
+  end
+end
